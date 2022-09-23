62235e3b3096fd23e881eb03b34cdaf232829ec2
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-09-24 00:40:40 +0600

    push

---
+++ Lab Task-1/Controllers/PortfolioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_Task_1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home() 
+        { 
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Project() 
+        {
+            return View();
+        }
+
+        public ActionResult Referace() 
+        { 
+            return View();
+        }
+    }
+}
