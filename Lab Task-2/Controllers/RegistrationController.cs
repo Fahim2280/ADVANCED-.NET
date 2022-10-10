d471cc4a5a4859676089235b07edfb3b61445873
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-10-10 23:25:57 +0600

    submit

---
+++ Lab Task-2/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using Lab_Task_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_Task_2.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(student std) 
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success", "Registration");
+            }
+            return View(std);
+        }
+
+        public ActionResult Success() 
+        {
+            return View();
+        }
+    }
+}
